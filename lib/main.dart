8d8af751334872e8cd2548662bdd27edf2141864
Saravanan <Sarafriends1986@gmail.com>
2022-05-02 16:31:18 +0800

    Flutter Code for Mobile UI development.

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sarauiapi/pages/passwordreset_page.dart';
+import 'package:sarauiapi/services/shared_service.dart';
+import 'package:sarauiapi/pages/login_page.dart';
+import 'package:sarauiapi/pages/register_page.dart';
+
+Widget _defaultHome = const Login();
+//Widget _defaultHome =  HomePage();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Get result of the login function.
+  bool _result = await SharedService.isLoggedIn();
+  if (_result) {
+    _defaultHome = const Login();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: const LoginPage(),
+      routes: {
+        '/': (context) => _defaultHome,
+        '/login': (context) => const Login(),
+        '/register': (context) => const Register(),
+        '/reset': (context) => const PasswordReset(),
+      },
+    );
+  }
+}
